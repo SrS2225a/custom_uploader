abfe1e4d8317d4afee84017f4af62500123a3a46
SrS2225a <sawyer.r.stephens@gmail.com>
2025-02-02 15:13:14 -0800

    hopefully fix flutter getting incorrect version number

--- lib/services/database.g.dart
+++ lib/services/database.g.dart
@@ -66,3 +66,40 @@ class ShareAdapter extends TypeAdapter<Share> {
           runtimeType == other.runtimeType &&
           typeId == other.typeId;
 }
+
+class ViewSelectionAdapter extends TypeAdapter<ViewSelection> {
+  @override
+  final int typeId = 1;
+
+  @override
+  ViewSelection read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return ViewSelection(
+      fields[0] as bool,
+      fields[1] as bool,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, ViewSelection obj) {
+    writer
+      ..writeByte(2)
+      ..writeByte(0)
+      ..write(obj.addNewView)
+      ..writeByte(1)
+      ..write(obj.shouldLoadPresets);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is ViewSelectionAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
